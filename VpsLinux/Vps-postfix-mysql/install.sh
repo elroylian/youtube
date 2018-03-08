d1c94d5cb1c775e188e1068b2d142787cd24b488
Fxstar <16883796+fxstar@users.noreply.github.com>
2018-03-08 13:39:13 +0100

    Postfix with mysql, imap, ssl and roundcube
    Postfix with mysql, imap, ssl and roundcube

--- VpsLinux/Vps-postfix-mysql/install.sh
+++ VpsLinux/Vps-postfix-mysql/install.sh
@@ -1,5 +1,8 @@
 # How to install postfix with mysql imap ssl
 https://linode.com/docs/email/postfix/email-with-postfix-dovecot-and-mysql/
 
-# Errors
+# roundcube webmail imap client
+https://linode.com/docs/email/postfix/email-with-postfix-dovecot-and-mysql/
+
+# Troubles
 https://www.youtube.com/watch?v=uhm6iPGXWHY
