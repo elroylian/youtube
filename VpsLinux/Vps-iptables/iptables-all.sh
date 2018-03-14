975cc4a458b90f850a19be9fd938f3566f5eb6ca
Fxstar <16883796+fxstar@users.noreply.github.com>
2018-03-14 11:00:40 +0100

    Linux iptables firewall
    Linux iptables firewall

--- VpsLinux/Vps-iptables/iptables-all.sh
+++ VpsLinux/Vps-iptables/iptables-all.sh
@@ -40,12 +40,11 @@ iptables -A INPUT -p tcp -m tcp --dport 993 -j ACCEPT
 iptables -A INPUT -p tcp -m tcp --dport 995 -j ACCEPT
 iptables -A INPUT -m state --state INVALID,NEW -j LOG --log-prefix "HACKER-INP "
 iptables -A INPUT -m state --state INVALID,NEW -j DROP
+iptables -A INPUT -m limit --limit 5/min -j LOG --log-prefix "DROP " --log-level 7
 iptables -A INPUT -j DROP
 iptables -A FORWARD -j LOG --log-prefix "FORWARD-ALL "
 iptables -A FORWARD -j LOG --log-prefix "FORWARD-ALL "
 iptables -A FORWARD -j DROP
-iptables -A INPUT -m limit --limit 5/min -j LOG --log-prefix "DROP " --log-level 7
-iptables -A INPUT -j DROP
 iptables -A OUTPUT -m limit --limit 5/min -j LOG --log-prefix "OUTPUT-ALL "
 
 # ip6tables v6
@@ -82,14 +81,12 @@ ip6tables -A INPUT -p tcp -m tcp --dport 25 -j ACCEPT
 ip6tables -A INPUT -p tcp -m tcp --dport 587 -j ACCEPT
 ip6tables -A INPUT -m state --state INVALID,NEW -j LOG --log-prefix "HACKER-INP6 "
 ip6tables -A INPUT -m state --state INVALID,NEW -j DROP
+ip6tables -A INPUT -m limit --limit 5/min -j LOG --log-prefix "DROP6 " --log-level 7
 ip6tables -A INPUT -j DROP
 ip6tables -A FORWARD -j LOG --log-prefix "FORWARD-ALL "
 ip6tables -A FORWARD -j LOG --log-prefix "FORWARD-ALL "
 ip6tables -A FORWARD -j DROP
-ip6tables -A INPUT -m limit --limit 5/min -j LOG --log-prefix "DROP6 " --log-level 7
-ip6tables -A INPUT -j DROP
 ip6tables -A OUTPUT -m limit --limit 5/min -j LOG --log-prefix "OUTPUT-ALL6 "
-
 
 # show in log each new connection 
 # grep "NEW-CONN" /var/log/kern.log | | wc -l
